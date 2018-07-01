4d91755de32966b09e6a951512c35bd1f8312f1b
Tushar <tushar987gupta@gmail.com>
2018-07-01 09:00:05 +0530

    Update settings to allow all hosts

--- finalYearProject/settings.py
+++ finalYearProject/settings.py
@@ -86,6 +86,14 @@ DATABASES = {
 }
 
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
